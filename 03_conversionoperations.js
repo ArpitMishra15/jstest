102d2d5fc4a866464acf1bcf83134a519afb434b
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-02-20 23:07:00 +0530

    add new file

---
+++ 03_conversionoperations.js
@@ -0,0 +1,12 @@
+//operations
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1"+ 2 + 2);
+
+// prefix and postfix
+//++X or X++
+let gameCounter = 100
+gameCounter++;
+
+console.log (gameCounter);
