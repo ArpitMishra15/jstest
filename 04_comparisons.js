9cefbb150b524984a06321f8bb0832655d5cfb6f
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-02-21 23:16:41 +0530

    add comarison file

---
+++ 04_comparisons.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// we usually avoid usong these
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// undefined will always be compared as false 
+console.log(undefined == 0);
+
+
+// === strictly checks, with the data type
+console.log("2" === 2);
