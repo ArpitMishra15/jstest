c5aca39779a34d151a9dae3c10c5d6c832ba4af7
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-02-26 01:04:42 +0530

    add new file

---
+++ 05_strings.js
@@ -0,0 +1,15 @@
+const name = "Arpit"
+const repoCount = "45"
+
+//console.log(name + repoCount + " Value");// nobody uses and likes this very often so avoid
+
+//correct way or more modern way
+
+console.log (`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// another way to declare a string(key value pairs in console; that doesnt mean array)
+const gameName = new String('Ngameiy')
+
+console.log(gameName[0]);             // to access key value pairs 
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase()); //to use the methods
