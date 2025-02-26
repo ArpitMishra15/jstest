ed30ec15f0ae5e870d417522c597b379e7e01435
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-02-27 00:58:03 +0530

    add new file

--- 05_strings.js
+++ 05_strings.js
@@ -12,4 +12,16 @@ const gameName = new String('Ngameiy')
 
 console.log(gameName[0]);             // to access key value pairs 
 console.log(gameName.__proto__);
-console.log(gameName.toUpperCase()); //to use the methods
+console.log(gameName.charAt(3)); //to use the methods
+console.log(gameName.indexOf('e')); //to use the methods
+
+const newString = gameName.substring(0, 4); // slicing(substring) of a string(you cant use negetive value; it ignores)
+console.log(newString);
+
+const anotherString = gameName.slice(0, 4) // slicing; can use negetive numbers
+console.log(anotherString);
+
+const newStringOne = "  arpit   "
+// to trim non required charecters like here; spaces
+console.log(newStringOne);
+console.log(newStringOne.trim());
