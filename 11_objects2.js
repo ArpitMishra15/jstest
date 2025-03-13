15156a93a7ce084c581589f7691fa47808ba83b9
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-03-14 00:57:32 +0530

    update file

--- 11_objects2.js
+++ 11_objects2.js
@@ -6,3 +6,25 @@ const tinderUser2 = {}
 
 console.log(tinderUser1);
 console.log(tinderUser2);
+
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser)
+
+//New way with object in object
+
+const regularUser = {
+    email : "spj@gmail.com",
+    fullName : {
+        userFullName : {
+            firstName: "Arpit",
+            lastName: "Mishra"
+        }
+    }
+}
+
+console.log(regularUser.fullName.userFullName.firstName);
