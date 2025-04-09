ab94888020da198be7f247e95ef20a44c9aeb366
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-04-09 13:47:51 +0530

    update previous & add new file

--- control_flow/one.js
+++ control_flow/one.js
@@ -30,3 +30,28 @@ const balance = 1000
 
     //if (balance > 500) console.log("run"), console.log("Run2")    // how not to write a code like this
 
+if (balance < 500) {
+    console.log("Less than 500");
+}else if (balance < 750) {
+    console.log("Less than 750");
+}else if (balance < 900) {
+    console.log("less than 900");
+}else {
+    console.log("Less than 1200");
+}
+
+// inclusion
+
+const userLoggedIn = true
+const debitCard = true
+const upiPayment = false
+const loggedInFromGoogle = true
+const loggedInFromEmail = false
+
+if ( userLoggedIn && debitCard || upiPayment ){                 // && is 'and' , || is 'or' 
+    console.log("Courses to buy");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in, Proceed to buy");
+}
