ee7af450d235ee095a6fa9b2e85b2d999ff7cafb
ArpitMishra15 <arpitmishra0315@gmail.com>
2025-04-10 23:27:11 +0530

    Updated file

--- iterations/one.js
+++ iterations/one.js
@@ -7,13 +7,35 @@ for (let i = 0; i < 11; i++) {
     }
     console.log(element);
 }
-//console.log(element)                                  //as element is not available outside scope, error persists
+//console.log(element)                                              //as element is not available outside scope, error persists
 
 for (let i = 0; i <= 5; i++) {
     console.log(`Outer loop value ${i}`);
     for (let j = 0; j < 4; j++) {
       console.log(`Inner loop value ${j} and inner loop ${i}`);
+      //console.log(i + '*' + j + '=' + i*j );                      // prints table
         
     }
     
 }
+
+let myArray = ["Superman", "Batman", "IronMan", "Spiderman" ]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+
+}
+
+
+// break & continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 6) {
+        console.log("Detected 6");
+        //break
+        //continue
+    }
+    console.log(`Value of i is ${index}`);
+}
+
